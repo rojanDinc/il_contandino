c11a462e9f395bbb8bca2e9757dbf421b4fd88fd
Rojan Dinc <rojan.dinc@valtech.se>
2020-06-13 11:32:09 +0200

    var: Updated food menu description text

--- src/partials/food-menu.js
+++ src/partials/food-menu.js
@@ -6,7 +6,7 @@ export default () => (
       <div id="menu-picture" className="picture-rounded" />
     </div>
     <div className="column">
-      <p className="p-5">På Restaurang Mama Rosa serverar vi rätter med autentiska smaker gjorda av noggrant utvalda färska ingredienser, modern matlagningsteknik och kortlivad hantverk. Genom att kombinera den unika mat- och dryckupplevelsen med en varm och personlig service vill vi bjuda in dig till oförglömliga, inspirerande och avkopplande möten, människor och mat.</p>
+      <p className="p-5">På Restaurang il Contadino serverar vi rätter med autentiska smaker gjorda av noggrant utvalda färska ingredienser, modern matlagningsteknik och kortlivad hantverk. Genom att kombinera den unika mat- och dryckupplevelsen med en varm och personlig service vill vi bjuda in dig till oförglömliga, inspirerande och avkopplande möten, människor och mat.</p>
       <div className="food-menu-body pt-15">
         <a className="p-5" target="blank" href="drinks_menu.png">Dryck</a>
         <a className="p-5" target="blank" href="pizza_menu.png">Pizza</a>
