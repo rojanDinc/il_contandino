4482701e625407b6556ffed70c52ace98ef16452
rojan-dinc <rojan.dinc@schibsted.com>
2021-06-05 00:27:32 +0200

    Updates

---
+++ cb1608f2-ca066b1083f070e1e14a.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{wHSu:function(c,n,o){"use strict";o.d(n,"a",(function(){return i}));var i={prefix:"fas",iconName:"chevron-down",icon:[448,512,[],"f078","M207.029 381.476L12.686 187.132c-9.373-9.373-9.373-24.569 0-33.941l22.667-22.667c9.357-9.357 24.522-9.375 33.901-.04L224 284.505l154.745-154.021c9.379-9.335 24.544-9.317 33.901.04l22.667 22.667c9.373 9.373 9.373 24.569 0 33.941L240.971 381.476c-9.373 9.372-24.569 9.372-33.942 0z"]}}}]);
+//# sourceMappingURL=cb1608f2-ca066b1083f070e1e14a.js.map
